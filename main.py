63eabc4602c4c6ea7a89a203d0c2347b902ce1b0
Juliantunes <Deabreuantj@gmail.com>
2024-03-18 13:48:17 -0400

    writing audio to wav file

---
+++ main.py
@@ -0,0 +1,26 @@
+import pyaudio
+import wave
+
+audio = pyaudio.PyAudio()
+
+stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+
+frames = []
+
+try:
+    while True:
+        data = stream.read(1024)
+        frames.append(data)
+except KeyboardInterrupt:
+    pass
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+# Use wave module to write the frames to a WAV file
+with wave.open("myrecording.wav", "wb") as sound_file:
+    sound_file.setnchannels(1)
+    sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+    sound_file.setframerate(44100)
+    sound_file.writeframes(b''.join(frames))
