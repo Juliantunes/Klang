c84772cea177cc81127d5a62d505b9e5b1991583
Juliantunes <Deabreuantj@gmail.com>
2024-03-18 14:13:51 -0400

    put wav through fft

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pyaudio
 import wave
+import numpy as np
+import scipy.fft
+
+
+
+
 
 audio = pyaudio.PyAudio()
 
@@ -18,9 +24,22 @@ stream.stop_stream()
 stream.close()
 audio.terminate()
 
+#concatenate frames into single byte string
+raw_data = b''.join(frames)
+
+format = pyaudio.paInt16
+x = np.frombuffer(raw_data, dtype=np.int16)
+y = scipy.fft(x)
+
+yinv = scipy.fft.ifft(y)
+
+
+
+
 # Use wave module to write the frames to a WAV file
 with wave.open("myrecording.wav", "wb") as sound_file:
     sound_file.setnchannels(1)
     sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
     sound_file.setframerate(44100)
     sound_file.writeframes(b''.join(frames))
+
